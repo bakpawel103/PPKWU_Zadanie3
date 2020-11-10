c07c1e7d988bb9fbcc23bf17bc8edd045383ff5a
pawelbak <215675@edu.p.lodz.pl>
2020-11-10 14:49:30 +0100

    Added get request to the root server site. Will display 'Welcome at site, that will generate ics file from weeia site'

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome at site, that will generate ics file from weeia site');
+});

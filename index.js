c1b6f3c84eef2fd0e1dca139bf46fe46e23db724
pawelbak <215675@edu.p.lodz.pl>
2020-11-10 15:35:08 +0100

    Started creating function for converting string html from weeia site response to json file.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const https = require('http');
+const http = require('http');
 const app = express();
 const port = 3000;
 
@@ -22,9 +22,39 @@ app.get('/getIcs', (req, res) => {
       (!req.body.lang || isNaN(parseInt(req.body.lang)))) {
     res.status(500).send("Pass body with string property");
   } else {
+    getCalendarFromUrl(req.body.rok, req.body.miesiac, req.body.lang);
+
     res.json({ result: req.body });
   }
 });
+
+var getCalendarFromUrl = (year, month, lang) => {
+  http.get(`http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=${year}&miesiac=${month}&lang=${lang}`, function(res) {
+    var data = [];
+
+    res.on('data', function(chunk) {
+      data.push(chunk);
+    }).on('end', function() {
+      let b = new Buffer(Buffer.concat(data).toString('base64'), 'base64')
+      var htmlString = b.toString();
+      var nodes = document.fromString(htmlString);
+      var result = tableToJson(nodes);
+    });
+  });
+}
+
+var tableToJson = (table) => {
+  var data = [];
+  for (var i=1; i<table.rows.length; i++) {
+    var tableRow = table.rows[i];
+    var rowData = [];
+    for (var j=0; j<tableRow.cells.length; j++) {
+      rowData.push(tableRow.cells[j].innerHTML);;
+    }
+    data.push(rowData);
+  }
+  return data;
+}
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
